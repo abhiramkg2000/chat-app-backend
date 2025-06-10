20f56d2179fe872c5df67e74bde9c806da186fdd
Abhiram Gopalakrishnan <abhiram.g@qburst.com>
2025-06-10 15:11:54 +0530

    added edit,delete message functionality

--- src/chat/chat-gateway.ts
+++ src/chat/chat-gateway.ts
@@ -8,8 +8,14 @@ import {
   ConnectedSocket,
 } from '@nestjs/websockets';
 import { Socket, Server } from 'socket.io';
+import { v4 as uuidv4 } from 'uuid';
 
-import { MessageListType, RoomUsersType } from 'src/types/commonTypes';
+import {
+  MessageType,
+  MessageListType,
+  RoomUsersType,
+} from 'src/types/commonTypes';
+
 import { roomIds } from 'src/constants/commonConstants';
 
 @WebSocketGateway({ cors: { origin: '*' } })
@@ -64,8 +70,8 @@ export class ChatGateway implements OnGatewayConnection, OnGatewayDisconnect {
     this.server.to(data.roomId).emit('users', this.roomUsers[data.roomId]);
   }
 
-  // USER MESSAGE
-  @SubscribeMessage('message')
+  // USER ADD MESSAGE
+  @SubscribeMessage('message:add')
   handleNewMessage(
     @MessageBody()
     data: {
@@ -83,9 +89,67 @@ export class ChatGateway implements OnGatewayConnection, OnGatewayDisconnect {
       clientId: client.id,
     });
 
-    this.server.to(data.roomId).emit('reply', data.message);
-    this.messages[data.roomId].push(data.message);
-    console.log('messages', this.messages);
+    const newMessage = {
+      ...data.message,
+      messageId: uuidv4(),
+      isEdited: false,
+      isDeleted: false,
+      editedAt: '',
+    };
+    this.server.to(data.roomId).emit('reply', newMessage);
+    this.messages[data.roomId].push(newMessage);
+    console.log('added messages', this.messages);
+  }
+
+  // USER EDIT MESSAGE
+  @SubscribeMessage('message:edit')
+  handleEditMessage(
+    @MessageBody()
+    data: {
+      roomId: string;
+      message: MessageType;
+    },
+    @ConnectedSocket() client: Socket,
+  ) {
+    // To stop typing event after the user edits message
+    this.server.to(data.roomId).emit('userStoppedTyping', {
+      clientId: client.id,
+    });
+
+    this.messages[data.roomId] = this.messages[data.roomId].map((message) => {
+      if (message.messageId === data.message.messageId) {
+        return {
+          ...data.message,
+        };
+      } else {
+        return message;
+      }
+    });
+
+    this.server.to(data.roomId).emit('prefetch', this.messages[data.roomId]);
+    console.log('edited messages', this.messages);
+  }
+
+  // USER DELETE MESSAGE
+  @SubscribeMessage('message:delete')
+  handleDeleteMessage(
+    @MessageBody()
+    data: {
+      roomId: string;
+      messageId: string;
+    },
+    // @ConnectedSocket() client: Socket,
+  ) {
+    this.messages[data.roomId] = this.messages[data.roomId].map((message) => {
+      if (message.messageId === data.messageId) {
+        return { ...message, isDeleted: true };
+      } else {
+        return message;
+      }
+    });
+
+    this.server.to(data.roomId).emit('prefetch', this.messages[data.roomId]);
+    console.log('deleted messages', this.messages);
   }
 
   // SOCKET CONNECTION
