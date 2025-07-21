7b1a5a1064caa29f0ead04a867a55e30ac1cabab
Abhiram Gopalakrishnan <abhiram.g@qburst.com>
2025-07-21 17:08:09 +0530

    added both local and deployed client origin

--- src/main.ts
+++ src/main.ts
@@ -6,12 +6,32 @@ import { ChatModule } from './chat/chat.module';
 async function bootstrap() {
   const app = await NestFactory.create(ChatModule);
 
+  // app.enableCors({
+  //   // allowedHeaders: '*',
+  //   allowedHeaders: ['Content-Type', 'ngrok-skip-browser-warning'],
+  //   // origin: '*',
+  //   origin: process.env.CLIENT_ORIGIN || 'http://localhost:3000', // frontend origin
+  //   credentials: true, // allow cookies to be sent
+  // });
+
+  const allowedOrigins = [
+    'http://localhost:3000',
+    'https://use-chat-app.netlify.app',
+  ];
+
   app.enableCors({
-    // allowedHeaders: '*',
+    origin: (
+      origin: string | undefined,
+      callback: (err: Error | null, allow?: boolean) => void,
+    ) => {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error('Not allowed by CORS'));
+      }
+    },
     allowedHeaders: ['Content-Type', 'ngrok-skip-browser-warning'],
-    // origin: '*',
-    origin: process.env.CLIENT_ORIGIN || 'http://localhost:3000', // frontend origin
-    credentials: true, // allow cookies to be sent
+    credentials: true,
   });
 
   app.use(cookieParser());
